a071159d5745ffce0c82b06f79822b80e3cb32a8
Yoel Aharoni <yoel345@gmail.com>
2021-10-28 15:15:51 +0300

    Add Jquery file and some functional to nav bar

---
+++ src/scripts/Jquey.js
@@ -0,0 +1,11 @@
+$('.account-list .menu .browse')
+    .popup({
+        inline: true,
+        hoverable: true,
+        position: 'bottom left',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    });
+$('.ui.dropdown').dropdown();
